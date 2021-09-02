db64cbd2c19dd633604f9524000c1892008c7971
kaseyk9 <23kkazliner@athenian.org>
2021-09-02 10:18:47 -0700

    wrote "hello world" code and added notes

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,5 +3,47 @@ public class MyMain {
         // Write some code here!
         System.out.println("Hello world!");
 
+        /* Java Basics:
+        Data types - Java requires all variables to have a declared type
+
+        Python: x=2 or y="hello" or z=True
+
+        Java:
+
+        int x = 2; // int is a whole number (integer)  x = x + 3
+
+        String y="hello"; // string is a collection of letters (words)
+
+        double z=1.2; // double is a decimal value
+
+        boolean a = true; // boolean is true or false
+
+        char b = 'f'; // char is a single letter
+
+        'a' vs "a" // single quotes are chars, double are strings
+
+        + operator:
+        1. add numbers together, int f = 1+2; // f=3
+        2. add strings together String s = "hello" + "world"; //s="helloworld"
+        3. add numbers and strings String t = "I like" + 7; //t="I like 7"
+
+        Converting types: int -> string
+        add to empty string ""
+        int x = 7;
+        String s = "" + x;
+        //s="7"
+
+        string -> int
+        use Integer.parse Int()
+        String y = "12";
+        int z = Integer.parseInt(y);
+        z//=12
+
+
+
+
+
+         */
+
     }
 }
